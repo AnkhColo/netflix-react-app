b27a9c93af2b1fb67e6139171b9c7776201ef735
“Colin <muriithicolin98@gmail.com>
2023-10-10 17:22:28 +0300

    Updates

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -8,8 +8,8 @@ const Main = () => {
     const movie = movies[Math.floor(Math.random() * movies.length)]
 
     useEffect( () => {
-        axios.get(requests.requestTopRated).then((response) => {
-          setMovies(response.data)
+        axios.get(requests.requestMovie).then((response) => {
+          setMovies(response.data.results)
         })
     },[])
         console.log(movie)
