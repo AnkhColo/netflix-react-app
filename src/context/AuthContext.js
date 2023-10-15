f9056c242b084f854e0400040b4bca109c6b4870
“Colin <muriithicolin98@gmail.com>
2023-10-15 08:53:18 +0300

    Updates02 - 15/10/23

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,31 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import {auth} from '../firebase'
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = createContext();
 
 export function AuthContexProvider({children}){
 
+    const [user, setUser] = useState({})
+
+    function signUp(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function LogIn(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function LogOut(){
+        return signOut(auth)
+    }
+
     return(
-        <AuthContex.Provider>
+        <AuthContex.Provider value={{signUp,user}}>
             ({children})
         </AuthContex.Provider>
     )
