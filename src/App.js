ee7e4662bfbea07a379692873f0839c6b6e2736f
“Colin <muriithicolin98@gmail.com>
2023-10-21 23:50:25 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Account from "./pages/Account";
+import { AuthContexProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <Navbar /> 
-      <Routes>
-        <Route path="/" element= {<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/account' element={<Account />} />
-      </Routes>
+    <AuthContexProvider>
+         <Navbar /> 
+           <Routes>
+              <Route path="/" element= {<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/account' element={<Account />} />
+           </Routes>
+    </AuthContexProvider>
       </>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,7 +6,7 @@ import {
     signOut, 
     onAuthStateChanged } from "firebase/auth";
 
-const AuthContext = createContext();
+const AuthContext = createContext()
 
 export function AuthContexProvider({children}){
 
