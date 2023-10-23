ea3933acc34185aa343f5f5f70ad25a2945836b0
“Colin <muriithicolin98@gmail.com>
2023-10-23 09:35:07 +0300

    Final stages!!

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,16 +5,16 @@ const Account = () => {
   return (
     <>
       <div className='w-full text-white'>
-      <img className='absolute w-full h-[400px] object-cover' 
+       <img className='absolute w-full h-[400px] object-cover' 
         src='https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/bdc6c8c9-c928-4353-b3d9-35f45ba46129/KE-en-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg' 
         alt='/'
         />
-        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px] '></div>
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
         <div className='absolute top-[20%] p-4 md:p-8'>
-          <h1 className='text-3xl md:test-5xl font-bold'>My Shows</h1>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+          <SavedShows />
         </div>
       </div>
-      <SavedShows />
     </>
   )
 }
